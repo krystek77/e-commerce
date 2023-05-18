f16c8fc28dc71f1a57e74ef7f22929474b88240b
krystek77 <krystian.wrona77@gmail.com>
2023-05-18 21:30:07 +0200

    Create search form

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import {SearchForm} from '@components'
 export default function Home() {
   return (
     <div className="flex flex-col flex-center">
       <h1 className="border-l-2 px-2 border-black mb-6">Home Page</h1>
+      <SearchForm/>
     </div>
   );
 }
---
+++ components/SearchForm/SearchForm.jsx
@@ -0,0 +1,24 @@
+import {BsSearch} from 'react-icons/bs';
+
+export default function SearchForm(){
+
+  return (
+    <div className='relative container'>
+      <form id='search' name='search' className='mx-4'>
+        <div className='absolute flex flex-col flex-center inset-y-0 left-4 rounded-tl-full rounded-bl-full px-4 font-inter text-md'>
+          <BsSearch className='w-4 h-4 text-black'/>
+        </div>
+        <input
+          className='border border-black-light rounded-full text-md shadow-md outline-none w-full py-2 pl-12 pr-20 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-black'
+          type='search'
+          placeholder='Wyszukaj częśc zamienną po nazwie'
+        />
+        <button
+          className='absolute inset-y-0 right-4 rounded-tr-full px-4 font-inter text-md bg-gold rounded-br-full outline-none'
+          type='submit'>
+          szukaj
+        </button>
+      </form>
+    </div>
+  );
+}
