99cd1a42b158bdc3cedce4f6240b7f9acb2beebc
krystek77 <krystian.wrona77@gmail.com>
2023-05-19 10:07:49 +0200

    Adjust advantages component styles to make it more responsive

--- components/Advantages/Advantages.jsx
+++ components/Advantages/Advantages.jsx
@@ -1,13 +1,29 @@
 import Link from 'next/link'
+import {RiStarSFill} from 'react-icons/ri'
+import {GiGoldBar} from 'react-icons/gi'
+import {FaTruck} from 'react-icons/fa'
+import {BsPhone} from 'react-icons/bs'
 
 const Advantages = () => {
   return (
     <div className='bg-slate-200 border-b border-t border-slate-300'>
       <div className="container mx-auto flex flex-col sm:flex-row justify-center items-center">
-        <Link href="/" className='w-full sm:w-1/4 text-center font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4 whitespace-nowrap'>Program lojalnościowy</Link>
-        <Link href="/" className='w-full sm:w-1/4 text-center font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4 whitespace-nowrap'>Nasza opinia na Opineo</Link>
-        <Link href="/" className='w-full sm:w-1/4 text-center font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4 whitespace-nowrap'>Dostawa od 0,00 PLN</Link>
-        <div className='w-full sm:w-1/4 text-center font-accent text-sm py-4 whitespace-nowrap'>tel. 602 191 607</div>
+        <Link href="/" className='w-full sm:w-1/4 flex sm:flex-row md:flex-col lg:flex-row justify-start px-8 sm:justify-center items-center flex-wrap font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4'>
+        <span className='mr-2'><GiGoldBar className='w-10 h-10 text-gold'/></span>
+        <b className='whitespace-nowrap'>Program lojalnościowy</b>
+        </Link>
+        <Link href="/" className='w-full sm:w-1/4 flex sm:flex-row md:flex-col lg:flex-row justify-start px-8 sm:justify-center items-center flex-wrap font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4'>
+        <span className='mr-2'><RiStarSFill className='w-10 h-10 text-accent-dark'/></span>
+        <span className='whitespace-nowrap'>Nasza opinia na Opineo</span>
+        </Link>
+        <Link href="/" className='w-full sm:w-1/4 flex sm:flex-row md:flex-col lg:flex-row justify-start px-8 sm:justify-center items-center flex-wrap font-accent text-sm  border-b sm:border-r sm:border-b-0 border-slate-300 py-4'>
+          <span className='mr-2'><FaTruck className='w-10 h-10 text-primary'/></span>
+          <span className='whitespace-nowrap'>dostawa <b>od 0,00 PLN</b></span>
+        </Link>
+        <div className='w-full sm:w-1/4 flex sm:flex-row md:flex-col lg:flex-row justify-start px-8 sm:justify-center items-center flex-wrap font-accent text-sm py-4'>
+          <span className='mr-2'><BsPhone className='h-10 w-10 text-black'/></span>
+          <span className='whitespace-nowrap'>tel. <b className='text-lg'>602 191 607</b></span>
+        </div>
       </div>
     </div>
   );
