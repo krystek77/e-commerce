efde1dc158f69e4c9984c63c5a6c5023d8df573a
krystek77 <krystian.wrona77@gmail.com>
2023-05-21 19:45:50 +0200

    add

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,15 +1,17 @@
-import {SearchForm} from '@components'
-import Image from 'next/image'
-import Link from 'next/link'
-import {images} from '@public/assets/images'
-import {BsFillPersonFill, BsFillCartFill} from 'react-icons/bs'
+import { SearchForm } from "@components";
+import Image from "next/image";
+import Link from "next/link";
+import { images } from "@public/assets/images";
+import { BsFillPersonFill, BsFillCartFill } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 const Header = () => {
   return (
-    <header className="px-4 lg:px-0">
+    <header className="py-8 lg:px-0">
       <div className="container mx-auto">
         {/** page tool */}
-        <div className="flex justify-between items-center py-8 flex-wrap">
-          <div className="hidden md:block flex-1">
+        <div className="flex justify-between items-center flex-nowrap px-4">
+          {/** logo type */}
+          <div className="hidden lg:block lg:flex-1">
             <Link href="/" className="block w-64 h-auto">
               <Image
                 className="object-cover w-full h-full object-center"
@@ -18,7 +20,8 @@ const Header = () => {
               />
             </Link>
           </div>
-          <div className="md:hidden flex-1">
+          {/** logo */}
+          <div className="lg:hidden">
             <Link href="/" className="block w-20 h-auto">
               <Image
                 className="object-cover w-full h-full object-center"
@@ -27,10 +30,18 @@ const Header = () => {
               />
             </Link>
           </div>
-          <div className='hidden lg:block flex-1'>
+          {/** hamburger */}
+          <button
+            type="button"
+            className="justify-self-center mx-4 flex justify-center rounded-md focus-element lg:hidden group"
+          >
+            <AiOutlineMenu className="w-10 h-10 text-black group-hover:text-black-light transition-element" />
+          </button>
+          {/** search form */}
+          <div className="hidden lg:block flex-1">
             <SearchForm />
           </div>
-          <div className="flex items-center flex-1 justify-end">
+          <div className="flex items-center justify-end lg:flex-1">
             <div className="whitespace-nowrap flex flex-col md:flex-row justify-center items-center pr-4 border-r border-slate-100">
               <div className="border border-accent-dark bg-accent p-1 rounded-full mr-2 mb-1 md:mb-0">
                 <BsFillPersonFill className="w-8 h-8 text-white" />
@@ -48,9 +59,10 @@ const Header = () => {
               </p>
             </div>
           </div>
-          <div className='lg:hidden w-full mt-4 lg:mt-0'>
-            <SearchForm />
-          </div>
+        </div>
+        {/** search form */}
+        <div className="lg:hidden w-full mt-2 lg:mt-0">
+          <SearchForm />
         </div>
       </div>
     </header>
