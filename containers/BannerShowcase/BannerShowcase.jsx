63af02c65e846c9d96a86a5057e9a9c6d095b20e
krystek77 <krystian.wrona77@gmail.com>
2023-05-24 12:47:17 +0200

    Create BannerShow component

--- containers/BannerShowcase/BannerShowcase.jsx
+++ containers/BannerShowcase/BannerShowcase.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {images} from '@public/assets/images'
+import Image from 'next/image';
 
 const BannerShowcase = props => {
   return (
-    <main className="bg-slate-50">
-      <div className="container mx-auto py-8 px-4 h-96 flex flex-center font-light text-xl ">BANNER SHOWCASE</div>
+    <main className="bg-slate-50 p-12">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col justify-between">
+          <div className="md:flex justify-between items-stretch mb-5">
+            <div className="border mb-5 md:mr-5 md:mb-0">
+              <Image src={images.DEFAULT_BANNER_630_630_WEBP} alt="" />
+            </div>
+            <div className="flex flex-col justify-between">
+              <div className="border mb-5 md:mb-0">
+                <Image src={images.DEFAULT_BANNER_630_400_WEBP} alt="" />
+              </div>
+              <div className="border">
+                <Image src={images.DEFAULT_BANNER_630_210_WEBP} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-[210px] border">
+          <Image src={images.DEFAULT_BANNER_1280_210_WEBP} alt="" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,13 +1,30 @@
 import DEFAULT_LANDSCAPE from './default_landscape_300_300._logo.webp';
 import DEFAULT_PORTRAIT from './default_portrait_300_300_logo.webp';
 import LOGO_BLACK_ON_TRANSPARENT from './logos/industrial-laundry-systems-low-resolution-logo-black-on-transparent-background.png'
-import LOGO_BLACK_ON_TRANSPARENT_ICON from './logos/industrial-laundry-systems-website-favicon-black.png'
+import LOGO_BLACK_ON_TRANSPARENT_ICON from './logos/industrial-laundry-systems-website-favicon-black.png';
+import DEFAULT_BANNER_630_630_WEBP from './banner_630_630.webp'
+import DEFAULT_BANNER_630_630_PNG from './banner_630_630.png'
+import DEFAULT_BANNER_630_400_WEBP from './banner_630_400.webp'
+import DEFAULT_BANNER_630_400_PNG from './banner_630_400.png'
+import DEFAULT_BANNER_630_210_WEBP from './banner_630_210.webp'
+import DEFAULT_BANNER_630_210_PNG from './banner_630_210.png'
+import DEFAULT_BANNER_1280_210_WEBP from './banner_1280_210.webp'
+import DEFAULT_BANNER_1280_210_PNG from './banner_1280_210.png'
+
 
 export const images = {
     DEFAULT_LANDSCAPE,
     DEFAULT_PORTRAIT,
     LOGO_BLACK_ON_TRANSPARENT,
-    LOGO_BLACK_ON_TRANSPARENT_ICON
+    LOGO_BLACK_ON_TRANSPARENT_ICON,
+    DEFAULT_BANNER_630_630_WEBP,
+    DEFAULT_BANNER_630_630_PNG,
+    DEFAULT_BANNER_630_400_WEBP,
+    DEFAULT_BANNER_630_400_PNG,
+    DEFAULT_BANNER_630_210_WEBP,
+    DEFAULT_BANNER_630_210_PNG,
+    DEFAULT_BANNER_1280_210_WEBP,
+    DEFAULT_BANNER_1280_210_PNG
 }
 
 export default images;
